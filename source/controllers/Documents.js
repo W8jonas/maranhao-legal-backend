3677820f8b6fcbe0fa7e70e8b21df860969b957c
Jonas <jonashenrique989@gmail.com>
2022-04-10 03:47:51 -0300

    alterando base de dados

--- source/controllers/Documents.js
+++ source/controllers/Documents.js
@@ -6,112 +6,112 @@ const admin = require("firebase-admin");
 
 const listaDeProcessos = [
     {
-        'assunto': 'DIREITO DO CONSUMIDOR (1156) - Contratos de Consumo (7771) - Bancário',
+        'subject': 'DIREITO DO CONSUMIDOR (1156) - Contratos de Consumo (7771) - Bancário',
         'cpf': '81109523050',
-        'classe judicial ': '[CÍVEL] PROCEDIMENTO COMUM CÍVEL',
-        'data da distribuição': '19/12/2014',
-        'jurisdicao': 'Betim',
-        'número processo': '6000871-19.2014.8.13.0027',
+        'judicialClass ': '[CÍVEL] PROCEDIMENTO COMUM CÍVEL',
+        'distributionDate': '19/12/2014',
+        'jurisdiction': 'Betim',
+        'processNumber': '6000871-19.2014.8.13.0027',
         'docs': [{
             id: '6000871-19.2014.8.13.0027',
         }],
-        'orgao julgador': '4ª Vara Cível da Comarca de Betim',
+        'judgingBody': '4ª Vara Cível da Comarca de Betim',
     },
     {
-        'número processo': '2361062-34.2014.8.13.0024',
+        'processNumber': '2361062-34.2014.8.13.0024',
         'docs': [{
             id: '2361062-34.2014.8.13.0024',
         }],
-        'data da distribuição': '19/08/2014',
-        'classe judicial ': '[CÍVEL] BUSCA E APREENSÃO EM ALIENAÇÃO FIDUCIÁRIA (81)',
-        'assunto': 'DIREITO CIVIL / Obrigações / Espécies de Contratos / Alienação Fiduciári',
+        'distributionDate': '19/08/2014',
+        'judicialClass ': '[CÍVEL] BUSCA E APREENSÃO EM ALIENAÇÃO FIDUCIÁRIA (81)',
+        'subject': 'DIREITO CIVIL / Obrigações / Espécies de Contratos / Alienação Fiduciári',
         'cpf': '01421708019',
-        'jurisdicao': 'Belo Horizonte',
-        'orgao julgador': '8ª Vara Cível da Comarca de Belo Horizonte',
+        'jurisdiction': 'Belo Horizonte',
+        'judgingBody': '8ª Vara Cível da Comarca de Belo Horizonte',
     },
     {
-        'número processo': '6000871-19.2014.8.13.0027',
+        'processNumber': '6000871-19.2014.8.13.0027',
         'docs': [{
             id: '6000871-19.2014.8.13.0027',
         }],
-        'data da distribuição': '19/12/2014',
-        'classe judicial ': '[CÍVEL] PROCEDIMENTO COMUM CÍVEL (7)',
-        'assunto': 'DIREITO DO CONSUMIDOR (1156) - Contratos de Consumo (7771) - Bancário',
+        'distributionDate': '19/12/2014',
+        'judicialClass ': '[CÍVEL] PROCEDIMENTO COMUM CÍVEL (7)',
+        'subject': 'DIREITO DO CONSUMIDOR (1156) - Contratos de Consumo (7771) - Bancário',
         'cpf': '72799560091',
-        'jurisdicao': 'Betim',
-        'orgao julgador': '4ª Vara Cível da Comarca de Betim',
+        'jurisdiction': 'Betim',
+        'judgingBody': '4ª Vara Cível da Comarca de Betim',
     },
     {
-        'número processo': '5005974-24.2015.8.13.0145',
+        'processNumber': '5005974-24.2015.8.13.0145',
         'docs': [{
             id: '5005974-24.2015.8.13.0145',
         }],
-        'data da distribuição': '27/10/2015',
-        'classe judicial ': '[CÍVEL] BUSCA E APREENSÃO EM ALIENAÇÃO FIDUCIÁRIA (81)',
-        'assunto': 'DIREITO CIVIL / Obrigações / Espécies de Contratos / Alienação Fiduciári',
+        'distributionDate': '27/10/2015',
+        'judicialClass ': '[CÍVEL] BUSCA E APREENSÃO EM ALIENAÇÃO FIDUCIÁRIA (81)',
+        'subject': 'DIREITO CIVIL / Obrigações / Espécies de Contratos / Alienação Fiduciári',
         'cpf': '87499889050',
-        'jurisdicao': 'Juiz de Fora',
-        'orgao julgador': '7ª Vara Cível da Comarca de Juiz de Fora',
+        'jurisdiction': 'Juiz de Fora',
+        'judgingBody': '7ª Vara Cível da Comarca de Juiz de Fora',
     },
     {
-        'número processo': '6015206-47.2015.8.13.0079',
+        'processNumber': '6015206-47.2015.8.13.0079',
         'docs': [{
             id: '6015206-47.2015.8.13.0079',
         }],
-        'data da distribuição': '04/11/2020',
-        'classe judicial ': '[CÍVEL] BUSCA E APREENSÃO EM ALIENAÇÃO FIDUCIÁRIA (81)',
-        'assunto': 'DIREITO CIVIL / Obrigações / Espécies de Contratos / Alienação Fiduciári',
+        'distributionDate': '04/11/2020',
+        'judicialClass ': '[CÍVEL] BUSCA E APREENSÃO EM ALIENAÇÃO FIDUCIÁRIA (81)',
+        'subject': 'DIREITO CIVIL / Obrigações / Espécies de Contratos / Alienação Fiduciári',
         'cpf': '85807514033',
-        'jurisdicao': 'Contagem',
-        'orgao julgador': '6ª Vara Cível da Comarca de Contagem',
+        'jurisdiction': 'Contagem',
+        'judgingBody': '6ª Vara Cível da Comarca de Contagem',
     },
     {
-        'número processo': '6122043-97.2015.8.13.0024',
+        'processNumber': '6122043-97.2015.8.13.0024',
         'docs': [{
             id: '6122043-97.2015.8.13.0024',
         }],
-        'data da distribuição': '12/11/2015',
-        'classe judicial ': '[CÍVEL] BUSCA E APREENSÃO EM ALIENAÇÃO FIDUCIÁRIA (81)',
-        'assunto': 'DIREITO CIVIL / Obrigações / Espécies de Contratos / Alienação Fiduciári',
+        'distributionDate': '12/11/2015',
+        'judicialClass ': '[CÍVEL] BUSCA E APREENSÃO EM ALIENAÇÃO FIDUCIÁRIA (81)',
+        'subject': 'DIREITO CIVIL / Obrigações / Espécies de Contratos / Alienação Fiduciári',
         'cpf': '41105557014',
-        'jurisdicao': 'Belo Horizonte',
-        'orgao julgador': '8ª Vara Cível da Comarca de Belo Horizonte',
+        'jurisdiction': 'Belo Horizonte',
+        'judgingBody': '8ª Vara Cível da Comarca de Belo Horizonte',
     },
     {
-        'número processo': '5005932-61.2016.8.13.0105',
+        'processNumber': '5005932-61.2016.8.13.0105',
         'docs': [{
             id: '5005932-61.2016.8.13.0105',
         }],
-        'data da distribuição': '29/09/2016',
-        'classe judicial ': '[CÍVEL] BUSCA E APREENSÃO EM ALIENAÇÃO FIDUCIÁRIA (81)',
-        'assunto': 'DIREITO CIVIL / Obrigações / Espécies de Contratos / Alienação Fiduciári',
+        'distributionDate': '29/09/2016',
+        'judicialClass ': '[CÍVEL] BUSCA E APREENSÃO EM ALIENAÇÃO FIDUCIÁRIA (81)',
+        'subject': 'DIREITO CIVIL / Obrigações / Espécies de Contratos / Alienação Fiduciári',
         'cpf': '68775625008',
-        'jurisdicao': 'Governador Valadares',
-        'orgao julgador': '7ª Vara Cível da Comarca de Governador Valadares',
+        'jurisdiction': 'Governador Valadares',
+        'judgingBody': '7ª Vara Cível da Comarca de Governador Valadares',
     },
     {
-        'número processo': '5006647-58.2016.8.13.0702',
+        'processNumber': '5006647-58.2016.8.13.0702',
         'docs': [{
             id: '5006647-58.2016.8.13.0702',
         }],
-        'data da distribuição': '17/06/2016',
-        'classe judicial ': '[CÍVEL] BUSCA E APREENSÃO EM ALIENAÇÃO FIDUCIÁRIA (81)',
-        'assunto': 'DIREITO CIVIL / Obrigações / Espécies de Contratos / Alienação Fiduciári',
+        'distributionDate': '17/06/2016',
+        'judicialClass ': '[CÍVEL] BUSCA E APREENSÃO EM ALIENAÇÃO FIDUCIÁRIA (81)',
+        'subject': 'DIREITO CIVIL / Obrigações / Espécies de Contratos / Alienação Fiduciári',
         'cpf': '83963477008',
-        'jurisdicao': 'Uberlândia',
-        'orgao julgador': '7ª Vara Cível da Comarca de Uberlândia',
+        'jurisdiction': 'Uberlândia',
+        'judgingBody': '7ª Vara Cível da Comarca de Uberlândia',
     },
     {
-        'número processo': '5012217-13.2016.8.13.0027',
+        'processNumber': '5012217-13.2016.8.13.0027',
         'docs': [{
             id: '5012217-13.2016.8.13.0027',
         }],
-        'data da distribuição': '28/09/2016',
-        'classe judicial ': '[CÍVEL] BUSCA E APREENSÃO EM ALIENAÇÃO FIDUCIÁRIA (81)',
-        'assunto': 'DIREITO CIVIL / Obrigações / Espécies de Contratos / Alienação Fiduciária',
+        'distributionDate': '28/09/2016',
+        'judicialClass ': '[CÍVEL] BUSCA E APREENSÃO EM ALIENAÇÃO FIDUCIÁRIA (81)',
+        'subject': 'DIREITO CIVIL / Obrigações / Espécies de Contratos / Alienação Fiduciária',
         'cpf': '67783111009',
-        'jurisdicao': 'Betim',
-        'orgao julgador': '5ª Vara Cível da Comarca de Betim',
+        'jurisdiction': 'Betim',
+        'judgingBody': '5ª Vara Cível da Comarca de Betim',
     },
 ]
 
@@ -131,11 +131,12 @@ async function getByCpf(cpf) {
     console.log('getByCpf', cpf)
 
     try {
-        // admin.firestore().collection("users").doc('Gbt1dLFOvZjzDuqMuWqu').set({
-        //     cpf: "123",
-        //     id: "Gbt1dLFOvZjzDuqMuWqu",
-        //     docs: listaDeIds
-        // })
+        listaDeProcessos.forEach(item => {
+            admin.firestore().collection("docs").doc(item.docs[0].id).set({
+                ...item,
+                docs: item.docs.map(doc => ({...doc, id: doc.id.replaceAll('.', '').replaceAll('-', '')}))
+            })
+        })
 
         const response = await admin.firestore().collection("users").where("cpf", "==", `${cpf}`).get();
         const data = response.docs.map((doc) => ({...doc.data(), id: doc.id}));
