94edd20a2e324a00aaa3be7f80cc3ed3ae172d43
Jonas <jonashenrique989@gmail.com>
2022-04-09 21:09:25 -0300

    criando VisualDesign  controller

--- source/controllers/VisualLaw.js
+++ source/controllers/VisualLaw.js
@@ -1,18 +1,39 @@
+const admin = require("firebase-admin");
+const fetch = require("node-fetch");
 
 
-async function getByCpf(id) {
-    console.log('getByCpf', id)
-    return {
-        status: '200',
-        data: {
-            dispositivo: 'Não definido ainda',
-            var2: 'Não definido ainda',
-            var3: 'Não definido ainda',
+async function getById(id) {
+    console.log('getById', id)
+
+	const options = {
+		method: "get",
+		headers: {"Accept": "application/json", "Content-Type": "application/json", "mode": "no-cors"},
+	};
+
+	try { 
+        const response = await admin.firestore().collection("docs").doc(id).get();
+        const data = response.data()
+
+		const response2 = await fetch(`https://maranhao-legal-pdf-api.herokuapp.com/categorias`, options);
+		const responseJson = await response2.json();
+
+		return {
+            status: '200',
+            data,
+            pdfData: {
+                ...responseJson.data
+            }
         }
-    }
+	} catch (error) {
+		console.log("Error - getById :", JSON.stringify({error, message: error.message}));
+        return {
+            status: '500',
+            error: JSON.stringify({error, message: error.message}),
+        }
+	}
 }
 
 
 module.exports = {
-    getByCpf
+    getById
 }
